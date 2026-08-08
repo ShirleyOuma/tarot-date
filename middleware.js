a5399f9130cd52344cc78d22a11a11786c29eb64
SHIRLEY <oumashirleyachieng@gmail.com>
2026-08-08 07:21:26 +0300

    Add explicit og:url tag to deck link previews

--- middleware.js
+++ middleware.js
@@ -67,6 +67,7 @@ export default async function middleware(request) {
   <meta charset="UTF-8" />
   <title>${title}</title>
   <meta property="og:title" content="${title}" />
+  <meta property="og:url" content="${escapeHtml(url.href)}" />
   <meta property="og:description" content="${description}" />
   <meta property="og:image" content="${image}" />
   <meta property="og:type" content="website" />
